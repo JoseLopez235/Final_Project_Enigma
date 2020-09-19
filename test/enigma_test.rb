0e9d4319cc6e3e00bb8b50076a06bc2589f331f9
Jose Lopez <joselopez11394@gmail.com>
2020-09-19 16:07:22 -0400

    adds decrypted letter finder test and adds more test for decrypt method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -15,6 +15,14 @@ class EnigmaTest < Minitest::Test
       date: "040895"
     }
     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+
+    key = enigma.stubs(:rand).returns(4321)
+    expected =  {
+      encryption: "ryqfytaiaei",
+      key: "04321",
+      date: "190920"
+    }
+    assert_equal expected, enigma.encrypt("hello world")
   end
 
   def test_should_return_random_key
@@ -90,7 +98,23 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.encrypted_hash_maker("keder ohulw", "02715", "040895")
   end
 
-  def test_should_decrypt_messagee
+  def test_decrypt_letter_finder
+    enigma = Enigma.new
+    character = "h"
+    shift = {A: 3}
+    key_sym = :A
+    assert_equal "e", enigma.decrypt_letter_finder(character, shift, key_sym)
+    character = "!"
+    shift = {A: 56}
+    key_sym = :A
+    assert_equal "!", enigma.decrypt_letter_finder(character, shift, key_sym)
+    character = "e"
+    shift = {A: 27}
+    key_sym = :A
+    assert_equal "e", enigma.decrypt_letter_finder(character, shift, key_sym)
+  end
+
+  def test_should_decrypt_message
     enigma = Enigma.new
     expected = {
       decryption: "hello world",
@@ -98,5 +122,12 @@ class EnigmaTest < Minitest::Test
       date: "040895"
     }
     assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+
+    expected =  {
+      decryption: " avxgwf jhn",
+      key: "02715",
+      date: "190920"
+    }
+    assert_equal expected, enigma.decrypt("hello world", "02715")
   end
 end
