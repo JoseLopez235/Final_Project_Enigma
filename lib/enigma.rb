8150fc5a4b8949d7c2d3c56f74a3b7c4e45328c6
Jose Lopez <joselopez11394@gmail.com>
2020-09-19 16:56:12 -0400

    moves encrypt letter finder to helper method module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,20 +8,6 @@ class Enigma < Shift
     @alphabet = ("a".."z").to_a << " "
   end
 
-  def encrypt_letter_finder(character, shift, key_sym)
-    if @alphabet.include?(character)
-      if shift[key_sym] == 27
-        character
-      else
-        letter_index = @alphabet.find_index(character)
-        letter_shift = @alphabet[(letter_index + shift[key_sym]) % 27]
-        letter_shift
-      end
-    else
-      character
-    end
-  end
-
   def encrypted_hash_maker(message, key, date)
     encrypted_hash = {}
     encrypted_hash[:encryption] = message
--- lib/helper_method.rb
+++ lib/helper_method.rb
@@ -1,3 +1,15 @@
 module HelperMethod
-
+  def encrypt_letter_finder(character, shift, key_sym)
+    if @alphabet.include?(character)
+      if shift[key_sym] == 27
+        character
+      else
+        letter_index = @alphabet.find_index(character)
+        letter_shift = @alphabet[(letter_index + shift[key_sym]) % 27]
+        letter_shift
+      end
+    else
+      character
+    end
+  end
 end
