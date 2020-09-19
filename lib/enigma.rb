662dbe4068fa8ca9357905586d735d09e8a719b8
Jose Lopez <joselopez11394@gmail.com>
2020-09-19 16:57:53 -0400

    adds decrypt letter finder into helper method module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -21,20 +21,6 @@ class Enigma < Shift
     encrypted_hash_maker(message, key, date)
   end
 
-  def decrypt_letter_finder(character, shift, key_sym)
-    if @alphabet.include?(character)
-      if shift[key_sym] == 27
-        character
-      else
-        letter_index = @alphabet.find_index(character)
-        letter_shift = @alphabet[(letter_index - shift[key_sym]) % 27]
-        letter_shift
-      end
-    else
-      character
-    end
-  end
-
   def decrypted_hash_maker(message, key, date)
     decrypted_hash = {}
     decrypted_hash[:decryption] = message
