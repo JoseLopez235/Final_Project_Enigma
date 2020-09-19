6f8f83d4824fad2404c357b6f23532b15f7261f5
Jose Lopez <joselopez11394@gmail.com>
2020-09-19 12:37:05 -0400

    adds final shift method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -35,7 +35,12 @@ class Enigma
   end
 
   def final_shift(key_hash, offset_hash)
-
+    final_shift = {}
+    final_shift[:A] = key_hash[:a_key].to_i+offset_hash[:a_offset].to_i
+    final_shift[:B] = key_hash[:b_key].to_i+offset_hash[:b_offset].to_i
+    final_shift[:C] = key_hash[:c_key].to_i+offset_hash[:c_offset].to_i
+    final_shift[:D] = key_hash[:d_key].to_i+offset_hash[:d_offset].to_i
+    final_shift
   end
 
   def encrypt(message, key=key_generator, date=date_generator)
