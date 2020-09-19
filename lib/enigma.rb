cb7d76360274c5b5cb67af54936fdc5694ea13e2
Jose Lopez <joselopez11394@gmail.com>
2020-09-19 16:33:15 -0400

    moved key codes method to shift class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,15 +6,6 @@ class Enigma < Shift
     @alphabet = ("a".."z").to_a << " "
   end
 
-  def key_codes(key)
-    key_hash = {}
-    key_hash[:a_key] = key[0]+key[1]
-    key_hash[:b_key] = key[1]+key[2]
-    key_hash[:c_key] = key[2]+key[3]
-    key_hash[:d_key] = key[3]+key[4]
-    key_hash
-  end
-
   def key_offsets(date)
     offset_hash = {}
     key = (date.to_i ** 2).to_s.chars.pop(4)
--- lib/shift.rb
+++ lib/shift.rb
@@ -8,4 +8,13 @@ class Shift
     date = Date.today
     date = date.strftime("%d%m%y")
   end
+
+  def key_codes(key)
+    key_hash = {}
+    key_hash[:a_key] = key[0]+key[1]
+    key_hash[:b_key] = key[1]+key[2]
+    key_hash[:c_key] = key[2]+key[3]
+    key_hash[:d_key] = key[3]+key[4]
+    key_hash
+  end
 end
